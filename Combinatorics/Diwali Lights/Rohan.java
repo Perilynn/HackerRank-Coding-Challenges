ed0be721a74f12ff923cb7c1f32d2c9ddc7214bd
Rohan Mathur <rohan@rmathur.com>
2015-05-21 00:32:41 -0400

    lol I bet python can't do this
    #java #cj #yee

--- Combinatorics/Diwali Lights/Rohan.java
+++ Combinatorics/Diwali Lights/Rohan.java
@@ -5,11 +5,7 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner keyboard = new Scanner(System.in);
-        int numOps = keyboard.nextInt();
-        while(numOps != 0) {
-            System.out.println((((keyboard.nextInt() * 2) - 1) % 100000));
-            numOps--;
-        }
+        Scanner keyboard = new Scanner(System.in); int numOps = keyboard.nextInt(); while(numOps != 0) { System.out.println((((keyboard.nextInt() * 2) - 1) % 100000)); numOps--; }
     }
 }
+
