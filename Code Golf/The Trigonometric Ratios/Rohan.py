658a418ee8541aa62dc2b8b6ffce6bb38fda0693
Rohan Mathur <rohan@rmathur.com>
2015-05-22 10:25:08 -0400

    Initial submission - Rohan

---
+++ Code Golf/The Trigonometric Ratios/Rohan.py
@@ -0,0 +1,7 @@
+for num in (range(input())):
+    x=input()
+    print round((x-((x**3)/6)+((x**5)/120)-((x**7)/5040)+((x**9)/362880)),3)
+    print round((1-((x**2)/2)+((x**4)/24)-((x**6)/720)+((x**8)/40320)),3)
+    
+# @score 0.5175
+# I will improve on this later
