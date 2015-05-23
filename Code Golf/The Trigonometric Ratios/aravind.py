7b671fc07092205a73e1ffee3656c3b809291073
Aravind Sundaresan <sundrsn2@illinois.edu>
2015-05-22 22:00:16 -0700

    best submission

--- Code Golf/The Trigonometric Ratios/aravind.py
+++ Code Golf/The Trigonometric Ratios/aravind.py
@@ -1,6 +1,2 @@
-input()
-while 1:
- a=input();b=[0,0];c=1
- for i in range(10):b[i%2]+=(-1)**(i/2)*a**i/c;c*=i+1
- print b[1],b[0]
-# score: 14.50
+gets;$<.map{|n|f=a=1;b=0;1.upto(9){|m|b,a=a,-b+f*=n.to_f/m};p a,b}
+# score: 16.70
