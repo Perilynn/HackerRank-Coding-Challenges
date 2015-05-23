0f42f9d613ba602a50df274d0833ac8964e00870
li-roy <royli2@illinois.edu>
2015-05-22 22:42:45 -0600

    Update roy.pl

--- Code Golf/The Trigonometric Ratios/roy.pl
+++ Code Golf/The Trigonometric Ratios/roy.pl
@@ -1,3 +1,5 @@
 $c=<>;while(<>){$,=0;$;=0;$~=$_;$t=5;$.=11;$_=1-$_*$~/$.--/$.*$~for($,,$;)x$t;printf'%f
 %f
 ',$~*$,,$;}
+#score: 14.90
+#a little help from http://codegolf.stackexchange.com/questions/27028/let-the-trigonometry-begin
