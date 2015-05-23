d7629e4a86cd5882b4bb08e65b6166b301362f8e
adn5327 <adn5327@users.noreply.github.com>
2015-05-23 07:51:06 +0530

    Update Abhishek.py

--- Code Golf/The Trigonometric Ratios/Abhishek.py
+++ Code Golf/The Trigonometric Ratios/Abhishek.py
@@ -1,6 +1,7 @@
-#SCORE 12.40
+#SCORE 12.80
 from math import *
-for i in range(input()):
+input()
+while 1:
     a=input(); b=[0,0]; 
     for j in range(10):b[j%2]+= (-1)**(j/2)*(a**j/factorial(j))
     print b[1],b[0]
