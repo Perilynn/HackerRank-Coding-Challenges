dcbada22ae6f4e19eb9b1685dd4fbf0dc7f0ddd7
Abhishek Modi <abhishek.modi95@gmail.com>
2015-06-04 01:00:58 -0500

    Submission for Grid Challenge

--- Greedy/Grid Challenge/Modi.py
+++ Greedy/Grid Challenge/Modi.py
@@ -1,50 +1,83 @@
 ## Solution for HackerRank Algorithms/Greedy/Grid Challenge
 ## https://www.hackerrank.com/challenges/grid-challenge
+
 __author__ = 'modi95'
 __date__ = 'Thursday 4 June 2015'
 
+
+# It seems one can rephrase this question as follows:
+#
+# Can each row be sorted such that:
+#   - Each row is in lexicographically sorted
+#   - Each column is lexicographically sorted
+#
+# Trivial Solution Run Time: (sqrt(n)(log n)
+#       Sorting each row = (sqrt(n)(log n)
+#       Checking columns at the end = n
+
 from sys import stdin
+
+square_size = 0
+matrix = []
+ith_soln = True
+
 
 
 def output_soln():
-    max = 0 
-    for set in set_sizes:
-        max = set_sizes[set] if (set_sizes[set] > max) else max
-
-    print max
+    global ith_soln
+    if ith_soln:
+        print("##########  YES")
+    else:
+        print("########### NO")
     return
 
-#returns list of lines containing orders
+def check_sort_cols():
+    global ith_soln
+    for i in range(0, square_size):
+        curr_char = matrix[0][i]
+        for j in range(0, square_size):
+            if (curr_char > matrix[j][i]):
+                ith_soln = False
+                return
+            curr_char = matrix[j][i]
+
+def sort_rows():
+    for row in matrix:
+        row.sort()
+        print row
+    return
+
 def get_inputs():
+    global square_size
+    global matrix
+    global ith_soln
 
-    line1 = stdin.readline()
-    rows = int(line1)
+    matrix = []
+    ith_soln = True
 
-    line2 = stdin.readline()
-    cols = int(line2)
+    line_in = stdin.readline()
+    square_size = int(line_in)
 
-    for i in range (0, rows):
-        line = stdin.readline()
-        line = line.replace(' ', '')
-        line = line.replace('\n', '')
-        matrix.append(list(line))
+    for i in range (0, square_size):
+        line_in = stdin.readline()
+        line_in = line_in.replace('\n', '')
+        matrix.append(list(line_in))
 
     return
 
 
 def main():
-    get_inputs()
-    global curr
-    
-    for y in range(0, rows):
-        for x in range (0, cols):
-            if (matrix[y][x] == '1'):
-                matrix[y][x] = curr
-                set_sizes[curr] = 1
-                curr = chr(ord(curr) + 1)
-                merge_around(y, x)
 
-    output_soln()
+    num_test_cases = int(stdin.readline())
+
+    while (num_test_cases > 0):
+        get_inputs()
+
+        sort_rows()
+        check_sort_cols()
+
+        output_soln()
+        num_test_cases -= 1
 
     return
 
